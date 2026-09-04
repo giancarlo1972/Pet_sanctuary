3652d09382fe332f5a1895da93d90bddf87ccdbe
Giancarlo <giancarlo.pereira@gmail.com>
2026-09-04 18:00:21 -0400

    Add Siren icon and update profile to reports tab
    Signed-off-by: Giancarlo <giancarlo.pereira@gmail.com>

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,5 +1,5 @@
 import { Tabs } from 'expo-router';
-import { Home, PawPrint, Users, User } from 'lucide-react-native';
+import { Home, PawPrint, Users, User, Siren } from 'lucide-react-native';
 import { Colors } from '@/constants/Colors';
 
 export default function TabLayout() {
@@ -37,11 +37,11 @@ export default function TabLayout() {
           tabBarIcon: ({ size, color }) => <Users size={size} color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="profile"
+     <Tabs.Screen
+        name="reports"
         options={{
-          title: 'Me',
-          tabBarIcon: ({ size, color }) => <User size={size} color={color} />,
+          title: 'Reports',
+          tabBarIcon: ({ size, color }) => <Siren size={size} color={color} />,
         }}
       />
     </Tabs>

ad1f6901bb0d3e3e709a1367de631beeec97f65f
Giancarlo <giancarlo.pereira@gmail.com>
2026-09-05 19:50:00 -0400

    Update versionCode to 10 in app.config.js
    Signed-off-by: Giancarlo <giancarlo.pereira@gmail.com>

--- app.config.js
+++ app.config.js
@@ -11,7 +11,7 @@ export default {
     ios: { supportsTablet: true },
     android: {
       package: "com.ruumatech.rescuearmy",
-      versionCode: 1,
+      versionCode: 10,
       adaptiveIcon: {
         foregroundImage: "./assets/icon.png",
         backgroundColor: "#1A1F3A",

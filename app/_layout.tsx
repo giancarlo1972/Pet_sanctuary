df55d86dd7a935a751833d12518df931bd912806
Giancarlo <giancarlo.pereira@gmail.com>
2026-09-06 17:15:16 -0400

    Register invoices route

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,6 +14,7 @@ export default function RootLayout() {
           <Stack.Screen name="admin" />
           <Stack.Screen name="pet-care" />
           <Stack.Screen name="updates" />
+          <Stack.Screen name="invoices" />
         </Stack>
         <StatusBar style="auto" />
       </>
